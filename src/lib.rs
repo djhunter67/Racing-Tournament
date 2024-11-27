2a0c54a016ec169b958d1b57efe415f3844d0901
Christerpher Hunter <djhunter67@gmail.com>
2024-11-26 20:24:38 -0500

    feat: actix init

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-pub mod seeding;
 pub mod models;
 pub mod odds;
+pub mod seeding;
+pub mod settings;
+pub mod startup;
+pub mod telemetry;
 pub mod timings;
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,63 @@
+use racing_tournament::{settings, startup::Application, telemetry};
+use std::io;
+use tracing::{error, info, warn};
+
 // use racing_tournament::
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    // This is a macro that allows for multiple loggers to be used at once
 
-    // Collect funds and apply discount for early payers
+    dotenv::dotenv().ok();
 
-    // Once the driver has paid, enter their name for random assignment
+    let mut settings = match settings::get() {
+        Ok(settings) => settings,
+        Err(err) => {
+            println!("Failed to load settings: {err}");
+            panic!("Failed to load settings");
+        }
+    };
 
-    // Calculate the payout for all indvididuals working for the organization and the gross income of the event
+    let subscriber = telemetry::get_subcriber(settings.clone().debug);
+    telemetry::init_subscriber(subscriber);
 
-    // Calculate the initial odds; the reference is each drivers performance at this event only
+    info!("Building the application");
+    let application = match Application::build(&mut settings).await {
+        Ok(app) => app,
+        Err(err) => {
+            error!("Failed to build application: {err}");
+            panic!("Failed to build application");
+        }
+    };
 
-    // Ensure each driver's car passes the technical safety inspection
+    info!("Listening on port: {}", application.port());
+    application.run_until_stopped().await?;
+    warn!("Shutting down");
 
-    // Seed the tournament
-
-    // Loop
-    // Take bets from the spectators; Calculate the house fee
-
-    // Race!
-
-    // Calculate bet payouts
-    // End Loop
-
-    // Reseed the bracket
+    Ok(())
 }
+
+// Collect funds and apply discount for early payers
+
+// Once the driver has paid, enter their name for random assignment
+
+// Calculate the payout for all indvididuals working for the organization and the gross income of the event
+
+// Calculate the initial odds; the reference is each drivers performance at this event only
+
+// Ensure each driver's car passes the technical safety inspection
+
+// Seed the tournament
+
+// Loop
+// Take bets from the spectators; Calculate the house fee
+
+// Race!
+
+// Calculate bet payouts
+// End Loop
+
+// Reseed the bracket
+
+// BFCSP
+// for $59 tuition for CS Prep? Until 12/2!
